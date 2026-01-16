e6d50470daffb8b7ee95e3e871c99a8cfd30c38b
Pahari47 <nachiketapahari00@gmail.com>
2026-01-16 10:45:57 +0530

    debugging

--- problem1/src/scraper/parseProducts.ts
+++ problem1/src/scraper/parseProducts.ts
@@ -1,27 +1,31 @@
 import * as Cheerio from "cheerio";
-import type { Product } from "../types/Product.js";
+import type { Product } from "../types/Product.ts";
 
 export function parseProducts(html: string): Product[] {
     const $ = Cheerio.load(html);
+    console.log("product-layout count:", $(".product-layout").length);
+    console.log("product-thumb count:", $(".product-thumb").length);
+    console.log("caption count:", $(".caption").length);
     const products: Product[] = [];
-
-    $(".product-thumb").each((_, element) => {
-        const name = $(element).find(".caption h4 a").text().trim();
-        const price = $(element).find(".price").first().text().trim();
-        const relativeUrl = $(element).find(".caption h4 a").attr("href");
-
-        if(name && price && relativeUrl) {
-            const priceNumber = parseFloat(price.replace(/[^\d.-]/g, ""));
-            
-            if (!isNaN(priceNumber)) {
-                products.push({
-                    name,
-                    price: priceNumber,
-                    url: relativeUrl.startsWith("http") ? relativeUrl : `https://mdcomputers.in/${relativeUrl}`
-                })
-            }
-        }
-    })
-    
+  
+    $(".product-layout").each((_, element) => {
+      const anchor = $(element).find(".caption h4 a");
+  
+      const name = anchor.text().trim();
+      const price = $(element).find(".price").first().text().trim();
+      const relativeUrl = anchor.attr("href");
+  
+      if (name && price && relativeUrl) {
+        products.push({
+          name,
+          price: price.replace(/\s+/g, " "),
+          url: relativeUrl.startsWith("http")
+            ? relativeUrl
+            : `https://mdcomputers.in/${relativeUrl}`
+        });
+      }
+    });
+  
     return products;
-}
+  }
+  
--- problem1/src/scraper/searchProducts.ts
+++ problem1/src/scraper/searchProducts.ts
@@ -1,11 +1,12 @@
-import type { Product } from "../types/Product.js";
-import { fetchHtml } from "./fetchHtml.js";
-import { parseProducts } from "./parseProducts.js";
+import type { Product } from "../types/Product.ts";
+import { fetchHtml } from "./fetchHtml.ts";
+import { parseProducts } from "./parseProducts.ts";
 
 export async function searchProducts(searchTerm: string): Promise<Product[]> {
     const encodedTerm = encodeURIComponent(searchTerm);
     const url = `https://mdcomputers.in/?route=product/search&search=${encodedTerm}`;
     const html = await fetchHtml(url);
+    console.log("HTML length:", html.length);
     
     return parseProducts(html);
 }
